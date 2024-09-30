3426b23afcb396814fb2c6f911c91432c557353e
Plasnid <stuart@sackler.ca>
2024-09-29 23:59:40 -0400

    code elements encapsulated within es5 classes

--- script.js
+++ script.js
@@ -1,78 +1,15 @@
-//* reference speed
-let speed = 50;
-
-//* reference for audio
-let footsteps = document.querySelector("#moveSound");
-let bonkers = document.querySelector("#bonk");
-
-//* reference the bounds of the play area;
-let playSpace = document.querySelector("#playSpace");
-
-//* reference the main character
-let hero = document.querySelector("#hero");
-let heroPos = hero.getBoundingClientRect();
-console.log(heroPos.width);
-let heroTop = (playSpace.clientHeight)/2;
-let heroLeft = (playSpace.clientWidth)/2;
-hero.style.top = `${heroTop}px`;
-hero.style.left = `${heroLeft}px`;
-
-//* keyboard commands - this is a function expression
-//* it does not get hoisted to the top of the function
-//* but it can be used as an IIFE(immediately invoked function expression)!
-let keyPressAction = (e) => {
-    switch(e.keyCode){
-        case 38:
-            moveDir({hs:0, vs:-1});
-            break;
-        case 40:
-            moveDir({hs:0, vs:1});
-            break;
-        case 37:
-            moveDir({hs:-1, vs:0});
-            break
-        case 39:
-            moveDir({hs:1, vs:0});
-            break;
-    }
+function Hero(moveSound, bonkSound, heroSpeed, heroIcon){
+    //* reference for audio
+    this.footsteps = document.querySelector(moveSound);
+    this.bonkers = document.querySelector(bonkSound);
+    //*hero speed referenc
+    this.heroSpeed = heroSpeed;
+    //* hero html Element
+    this.heroIcon = document.querySelector(heroIcon);
 }
 
-//*capture key presses
-document.addEventListener("keydown", keyPressAction);
-//* this is a function declaration, it gets hoisted to the top
-function moveDir(motionDir){
-    newLeft = heroLeft + (speed*motionDir.hs);
-    newTop =  heroTop + (speed*motionDir.vs);
-    let playBonk = false;
-    if(newLeft>0 && newLeft<playSpace.clientWidth - (heroPos.width)){
-        heroLeft = newLeft;
-    }else if(newLeft<=0){
-        heroLeft = 0;
-        playBonk = true;
-    }else{
-        heroLeft = playSpace.clientWidth-heroPos.width;
-        playBonk = true;
-    }
-    if(newTop>0 && newTop<playSpace.clientHeight - (heroPos.width/2)){
-        heroTop = newTop;
-    }else if(newTop<=0){
-        heroTop = 0;
-        playBonk = true;
-    }else{
-        heroTop = playSpace.clientHeight-heroPos.height;
-        playBonk = true;
-    }
-    if(playBonk==true){
-        bonkers.currentTime = 0;
-        bonkers.play();
-    }else{
-        footsteps.currentTime = 0;
-        footsteps.play();
-    }
-    gsap.to(hero, { backgroundColor:randoCol(), top: heroTop, left:heroLeft, duration: .5 });
-}
 //* random colours tool
-let randoCol = function(){
+Hero.prototype.randoCol = function(){
     let letters = '0123456789ABCDEF';
     let color = '#';
     for (let i = 0; i < 6; i++) {
@@ -83,3 +20,93 @@ let randoCol = function(){
     console.log(`%c${color}%c is the %c color! %c Woot!`, style, "font-size: 2em;",style,"font-size: 3em;");
     return color;
 }
+
+function GameBoard(playSpace, gameHero){
+    //* reference the bounds of the play area;
+    this.playSpace = document.querySelector(playSpace);
+    //* reference to the hero html element
+    this.gameHero = gameHero;
+    //* Hero Icon Bounding Rectangle
+    this.heroPos = this.gameHero.heroIcon.getBoundingClientRect();
+    this.startGame();
+}
+
+//* this is a function declaration, it gets hoisted to the top
+GameBoard.prototype.moveDir = function(motionDir){
+    let newLeft = this.heroLeft + (this.gameHero.heroSpeed*motionDir.hs);
+    let newTop =  this.heroTop + (this.gameHero.heroSpeed*motionDir.vs);
+    let playBonk = false;
+    if(newLeft>=0 && newLeft<this.playSpace.clientWidth - (this.heroPos.width)){
+        this.heroLeft = newLeft;
+    }else if(newLeft<0){
+        console.log("far left");
+        this.heroLeft = 0;
+        playBonk = true;
+    }else{
+        console.log("far right")
+        this.heroLeft = this.playSpace.clientWidth-this.heroPos.width;
+        playBonk = true;
+    }
+    console.log(this.heroLeft);
+    if(newTop>=0 && newTop<playSpace.clientHeight - (this.heroPos.width/2)){
+        this.heroTop = newTop;
+    }else if(newTop<0){
+        this.heroTop = 0;
+        playBonk = true;
+    }else{
+        this.heroTop = playSpace.clientHeight-this.heroPos.height;
+        playBonk = true;
+    }
+
+    if(playBonk==true){
+        this.gameHero.bonkers.currentTime = 0;
+        this.gameHero.bonkers.play();
+    }else{
+        this.gameHero.footsteps.currentTime = 0;
+        this.gameHero.footsteps.play();
+    }
+    gsap.to(this.gameHero.heroIcon, { backgroundColor:this.gameHero.randoCol(), top: this.heroTop, left:this.heroLeft, duration: .5 });
+}
+
+//* keyboard commands
+GameBoard.prototype.keyPressAction = function(e){
+    console.log(this);
+    switch(e.keyCode){
+        case 38:
+            this.moveDir({hs:0, vs:-1});
+            break;
+        case 40:
+            this.moveDir({hs:0, vs:1});
+            break;
+        case 37:
+            this.moveDir({hs:-1, vs:0});
+            break
+        case 39:
+            this.moveDir({hs:1, vs:0});
+            break;
+    }
+}
+
+GameBoard.prototype.startGame = function(){
+    //* position the hero on the screen;
+    this.heroTop = (this.playSpace.clientHeight)/2;
+    this.heroLeft = (this.playSpace.clientWidth)/2;
+    //console.log(this.playSpace.clientHeight)
+    this.gameHero.heroIcon.style.top = `${this.heroTop}px`;
+    this.gameHero.heroIcon.style.left = `${this.heroLeft}px`;
+
+    //*self captures reference to the Gameboard class
+    let self = this;
+    //document.addEventListener("keydown", self.keyPressAction);
+    //* whole window listens to the keydown event
+    //* a closure is formed here where self is 
+    window.addEventListener('keydown', function(event) {
+        console.log(self);
+        console.log(this);
+        self.keyPressAction(event);
+        // self is your Keyboard object. You can refer to all your properties from this
+    });
+}
+
+let gameHero = new Hero("#moveSound", "#bonkSound",50, "#hero");
+let gameBoard = new GameBoard("#playSpace", gameHero);
